5f91b84fdb3477711ef640d5b900590fe0da4bd8
r1c0n <reconispog@gmail.com>
2023-01-21 22:20:05 -0800

    Color code header file

---
+++ color_codes.h
@@ -0,0 +1,22 @@
+// color_codes.h
+// this file is where the variables for different color codes are stored
+
+#pragma once
+
+constexpr char Black[] = "\033[22;30m";
+constexpr char Red[] = "\033[22;31m";
+constexpr char Green[] = "\033[22;32m";
+constexpr char Brown[] = "\033[22;33m";
+constexpr char Blue[] = "\033[22;34m";
+constexpr char Purple[] = "\033[22;35m";
+constexpr char Cyan[] = "\033[22;36m";
+constexpr char Gray[] = "\033[22;37m";
+constexpr char Dark_Gray[] = "\033[01;30m";
+constexpr char Bright_Red[] = "\033[01;31m";
+constexpr char Bright_Green[] = "\033[01;32m";
+constexpr char Yellow[] = "\033[01;33m";
+constexpr char Bright_Blue[] = "\033[01;34m";
+constexpr char Bright_Purple[] = "\033[01;35m";
+constexpr char Bright_Cyan[] = "\033[01;36m";
+constexpr char White[] = "\033[01;37m";
+constexpr char Reset[] = "\033[0m";
